358942b27e5ee7758b1484f965074def43a0fa5f
ghadyg <ghoshghady22@gmail.com>
2024-06-24 19:58:49 +0300

    adding all files

---
+++ public/content.js
@@ -0,0 +1,25 @@
+
+
+// // Create an async function to handle asynchronous code
+// async function addTabUrlsToPopup() {
+//     try {
+        
+//         console.log(tabs);
+        
+//     } catch (error) {
+//         console.error('Error querying tabs:', error);
+//     }
+
+//     // Append popup to the body
+//     document.body.appendChild(popup);
+
+//     // Automatically remove the popup after a few seconds
+//     setTimeout(() => {
+//         if (document.body.contains(popup)) {
+//             document.body.removeChild(popup);
+//         }
+//     }, 50000); // Change this value to keep the popup longer or shorter
+// }
+
+// // Call the async function
+// addTabUrlsToPopup();
---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [color, setColor]= useState("");
+  const clickMe = async() =>{
+    let arr = await chrome.tabs.query({active: true});
+    arr.forEach( tab =>{
+    chrome.scripting.executeScript({
+      target:{tabId: tab.id},
+      args:[color],
+      func: (color)=>{
+        document.body.style.backgroundColor = color;
+      }
+
+    })
+
+    })
+  }
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <input type='color' onChange={(e)=>(setColor(e.currentTarget.value))} value={color}/>
+        <button onClick={clickMe}>
+          your color is {color}
+        </button>
+        <p>
+           {color}
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
